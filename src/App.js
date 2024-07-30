a5c55d404e6cba92c7619e19118421798153df0f
sostonoyan <sos.tonoyan-extern@invia.de>
2024-07-30 12:42:11 +0400

    image styles added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CountdownTimer from './CountdownTimer';
+import kidsImage from './kids.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="invitation-card">
+      <h1>Our Wedding Invitation</h1>
+      <div className="container">
+        <div className="image-container">
+          <img src={kidsImage} alt="Wedding" className="invitation-image" />
+        </div>
+        <div style={{marginLeft: "20px"}}>
+          <p>Սիրով  հրավիրում  ենք  Ձեզ  ներկա  գտնվելու  մեր  սիրելի  դստրեր՝  Մարիի  և  Ալբինայի  Սուրբ  Մկրտության  արարողությանը
+            Մկրտության  արարողությունը  տեղի  կունենա  15.08.2024թ. ժամը  15:30-ին  Սուրբ Երրորդություն  եկեղեցի  ք.Երևան, Րաֆֆու փողոց
+            Մկրտության  տոնակատարությունը  տեղի  կունենա  ժամը  17:00-ին  Էլիտա  ռեստորան  ք.Երևան, Անդրանիկի փ. 15/2
+            Սիրով  կսպասենք  Ձեզ  միասին  նշանավորելու  նրանց  հոգևոր  ճանապարհորդության սկիզբը:
+            Հարգանքով՝  Բաբայանների  ընտանիք</p>
+          <CountdownTimer />
+        </div>
+      </div>
     </div>
   );
 }
